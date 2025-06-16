c57905cf1afa877ebfaf91e1f9f7535b835ee044
SomanathGavade172 <somanathgavade2001@gmail.com>
2025-06-16 23:33:50 +0530

    Update Program16_01.py

--- Assignment_16/Program16_01.py
+++ Assignment_16/Program16_01.py
@@ -2,21 +2,44 @@
     1. Write a Python program to create a text file named student.txt and write the names of 5 students into it.
 
 '''
+# Import Module os
+import os
 
-def create_file(filename):
-    fobj = open(filename, 'w')
+# function Defination
+def CreateFile(FName):
+    # Open the File in Write Mode
+    fobj = open(FName, "w")
     
-    print("Enter name of the 5 students: ")
+    print("Enter 5 Students Name : ")
+    
+    # Accept 5 Name of Student from User
     for i in range(5):
-        name = input()
-        fobj.write(name+'\n')
+        Name = input()
 
+        # Write Student Name Into the File
+        fobj.write(Name + "\n")
+
+    # File is Close
     fobj.close()
 
-    print(f"Student names added to the {filename} successfully.")
+    print("Students Name Write into the File : ", FName)
 
+# Main functions
 def main():
-    create_file("student.txt")
+    print("Enter File Name that you want to Create : ")
 
+    # Accept File Name form User
+    FileName = input()
+
+    # Check File is Allready Created
+    bRet = os.path.exists(FileName)
+
+    if(bRet == False):
+        # Function Call
+        CreateFile(FileName)
+    else:
+        print("File is All Ready Created ..! Please Create another File")
+
+# Starter
 if __name__ == "__main__":
-    main()
+    main()                  # Function Call
